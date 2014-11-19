1193e2da0891bc8716512a18e2e99667391c9427
Chris Barker <cbarker@puppetlabs.com>
2014-11-19 14:41:09 -0800

    fix sshd config for now
    Not secure, but we can't do ssh keys in guacamole yet

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -38,6 +38,20 @@ class portapack::nodes (
         key     => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQC0KvgeiOT1kWvfP8FrzDi3W3QuQinesu//h+MA9Omxx9EF38hX1aHX8XQk79jAxrRkKMyUq/h45ATxv2Qbr83uW1qZ9tSD//8FeRw0V5oilOhQHRQKL4EVK2BG5yR0k2Td19ptPCXvBOQ2YMJg7aLC4im6smAVrwyQcMnYWfR8uneof4GQhwNjU0cmZPb1J5dXS2kYYTcv1mmxe+d7EXkbh1ms+1wIDyvZWYsaYPrVgVeznHKD5I56a67AeFhccShn4rkDIATL1UNmfnZSg5QOLKyYmTxSmvov0maR2/zn7SKf5j7E1kq+kJ+xTcd5ooRRUBrYGsEsTAW8kQlJRAEB',
         require => User[$student],
       }
+      
+      file { '/etc/ssh/sshd_config':
+        ensure => file,
+        owner  => root,
+        group  => root,
+        mode   => 0644,
+        notify => Service['sshd'],
+      }
+
+      service { 'sshd':
+        ensure => running,
+        enable => true,
+      }
+
     }
   }
 }
